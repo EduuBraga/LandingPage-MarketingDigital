df9de95f539739fb81a9eebdf976bdde89fddb63
EduuBraga <chocolate.blindado123@gmail.com>
2022-08-02 16:41:06 -0300

    Corrigindo responsividade
    neste commit foi corrigido a responsividade além da adição da animação do scroll

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,10 @@
+// Accordion =>
 const faqs = document.querySelectorAll(".card-faq")
 
-faqs.forEach(faq =>{
+faqs.forEach(faq => {
     const faqPergunta = faq.querySelector(".card-faq-pergunta")
-    
-    faqPergunta.addEventListener('click', ()=>{
+
+    faqPergunta.addEventListener('click', () => {
         const faqResposta = faq.querySelector(".card-faq-resposta")
 
         const respostaAtiva = document.querySelector(".ativo")
@@ -11,21 +12,35 @@ faqs.forEach(faq =>{
     })
 })
 
-function verificarAtivo(faqPergunta, faqResposta, respostaAtiva){
+function verificarAtivo(faqPergunta, faqResposta, respostaAtiva) {
     const imgFaq = faqPergunta.querySelector(".img-faq")
     const imgsFaq = document.querySelectorAll(".img-faq")
 
-    imgsFaq.forEach(img =>{img.style = "transform: rotate(0deg);"})
+    imgsFaq.forEach(img => { img.style = "transform: rotate(0deg);" })
 
-    if(respostaAtiva){
+    if (respostaAtiva) {
         respostaAtiva.style = "height: 0;"
         respostaAtiva.classList.remove("ativo")
         imgFaq.style = "transform: rotate(0deg);"
     }
 
-    if(faqResposta !== respostaAtiva){
+    if (faqResposta !== respostaAtiva) {
         faqResposta.classList.add("ativo")
         faqResposta.style.height = faqResposta.scrollHeight + 10 + "px"
         imgFaq.style = "transform: rotate(180deg);"
     }
+    console.log(respostaAtiva)
+    console.log(faqResposta)
 }
+
+// Animação scroll
+window.sr = ScrollReveal({ reset: true })
+
+sr.reveal('.card-depoimentos', {
+    rotate: { x: 0, y: 100, z: 0 },
+    duration: 2000,
+})
+sr.reveal('.card-beneficios', {
+    rotate: { x: 0, y: 100, z: 0 },
+    duration: 2000,
+})
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-## Landing Page - Curso marketing digital
+# Landing Page - Curso marketing digital
+
+<p>Landing page feita com Bootstrap, CSS3, HTML5 e JavaScript. Trata-se de uma página de vendas de um curso de marketing digital fictício.<br>
+No projeto foi abordado elementos animados, como botões e animações no scroll consumindo a API scrollreveal. Além de utilizar o JS para fazer um FAQ em accordion.<br>
+O projeto inteiro está responsivo, então ele se adapta a qualquer tela, como smartphones, tablets e notebooks. 
+</p>
