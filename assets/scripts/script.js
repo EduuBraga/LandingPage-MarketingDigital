0b81c4d9595fdcc83af3dea178e9b27158822d64
EduuBraga <chocolate.blindado123@gmail.com>
2022-08-02 11:50:04 -0300

    Revisão do código e Accordion
    Neste commit eu adicionei o accordion as FAQs e revisei o código retirando o que não necessitava e organizando

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,31 @@
 const faqs = document.querySelectorAll(".card-faq")
 
 faqs.forEach(faq =>{
-    faq.addEventListener('click', ()=>{
-        faq.classList.toggle("ativo")
+    const faqPergunta = faq.querySelector(".card-faq-pergunta")
+    
+    faqPergunta.addEventListener('click', ()=>{
+        const faqResposta = faq.querySelector(".card-faq-resposta")
+
+        const respostaAtiva = document.querySelector(".ativo")
+        verificarAtivo(faqPergunta, faqResposta, respostaAtiva)
     })
 })
+
+function verificarAtivo(faqPergunta, faqResposta, respostaAtiva){
+    const imgFaq = faqPergunta.querySelector(".img-faq")
+    const imgsFaq = document.querySelectorAll(".img-faq")
+
+    imgsFaq.forEach(img =>{img.style = "transform: rotate(0deg);"})
+
+    if(respostaAtiva){
+        respostaAtiva.style = "height: 0;"
+        respostaAtiva.classList.remove("ativo")
+        imgFaq.style = "transform: rotate(0deg);"
+    }
+
+    if(faqResposta !== respostaAtiva){
+        faqResposta.classList.add("ativo")
+        faqResposta.style.height = faqResposta.scrollHeight + 10 + "px"
+        imgFaq.style = "transform: rotate(180deg);"
+    }
+}
--- readme.md
+++ readme.md
@@ -1,12 +1,1 @@
-#### Paleta de cores 
-principal   #1d6b87
-secundaria  #3788a6
-tercearia   #0a5773
-
-cor da logo #009995
-cor secundaria da logo #0a5773
-
-logos e botao   #15b0ab, #22d9d3, #49faf4 (da escura pra mais clara)
-
-LARANJA #f86b00
-AMARELO #ffbd35
+## Landing Page - Curso marketing digital
